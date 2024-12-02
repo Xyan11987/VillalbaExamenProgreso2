1b80b8ace932b34eb13ab72bd5f9ff24b2789680
laboratorio <laboratorio@UPOAULA10217>
2024-12-02 07:14:57 -0500

    Agregar archivos de proyecto.

---
+++ VillalbaExamenProgreso2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VillalbaExamenProgreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VillalbaExamenProgreso2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VillalbaExamenProgreso2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
